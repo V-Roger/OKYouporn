16960b6253f68f484c7c52a91f894706aa563fa8
Virgil Roger <roger.virgil@gmail.com>
2017-05-29 15:04:31 +0200

    🚧 Yomama so rude she don't get past Chrome

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
     'use strict';
 
     var okyouporn = okyouporn || {};
+    var api = artyom.getNativeApi();
+
 
     artyom.on(['OK Youporn *'], true).then((i, wildcard) => {
         window.open('https://www.youporn.com/search/?query=' + wildcard);
@@ -11,8 +13,10 @@
         lang:"fr-FR",
         debug:true, // Show what recognizes in the Console
         listen:true, // Start listening after this
-        speed:0.9, // Talk a little bit slow
-        mode:"normal" // This parameter is not required as it will be normal by default
+        speed:1, // speech speed
+        soundex: true, // Use the soundex algorithm to increase accuracy
+        continuous: true, // listen forever and ever and ever       
+        mode:"normal", // This parameter is not required as it will be normal by default
     });
 
 
