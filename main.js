4cca29dbe2d6797b2247715cbef5cf406190fabb
Virgil Roger <roger.virgil@gmail.com>
2017-06-06 16:20:40 +0200

    ✨ Dirty "youporn" recognition, can't figure out how to remove profanity filter of chrome speech API

--- main.js
+++ main.js
@@ -1,24 +1,29 @@
 (function () {
     'use strict';
 
-    var okyouporn = okyouporn || {};
-    var api = artyom.getNativeApi();
+    artyom.on(['ok *'] , true).then((i,wildcard) => {
+        let words = wildcard.split(" ");
+        let trigger = words.indexOf("y******");
+        if(trigger !== -1) {
+            words = words.splice(trigger + 1);
+            let request = words.reduce((acc, val) => acc + "+" + val, "");
 
+            window.open('https://www.youporn.com/search/?query=' + request);
+        }
+    }); 
 
-    artyom.on(['OK Youporn *'], true).then((i, wildcard) => {
-        window.open('https://www.youporn.com/search/?query=' + wildcard);
-    });
-
+    // Start the commands !
     artyom.initialize({
-        lang:"fr-FR",
-        debug:true, // Show what recognizes in the Console
-        listen:true, // Start listening after this
-        speed:1, // speech speed
-        soundex: true, // Use the soundex algorithm to increase accuracy
-        continuous: true, // listen forever and ever and ever       
-        mode:"normal", // This parameter is not required as it will be normal by default
+        lang: "fr-FR", // GreatBritain english
+        continuous: true, // Listen forever
+        soundex: true,// Use the soundex algorithm to increase accuracy
+        debug: false, // Show messages in the console
+        listen: true // Start to listen commands !
+    }).then(() => {
+        console.log("Artyom has been succesfully initialized");
+    }).catch((err) => {
+        console.error("Artyom couldn't be initialized: ", err);
     });
-
 
 }) ();
 
